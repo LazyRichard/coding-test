ed954eef6db4d8ec926490b7c523d08cdc9cc057
Suh Junmin <sof.midnight@live.co.kr>
2020-04-02 04:10:18 +0900

    fix tests

--- programmers/programmers_42840/__init__.py
+++ programmers/programmers_42840/__init__.py
@@ -1,13 +1,13 @@
 from typing import List, Iterable
 from itertools import cycle
 
-pattern_1: Iterable[int] = cycle([1, 2, 3, 4, 5])
-pattern_2: Iterable[int] = cycle([2, 1, 2, 3, 2, 4, 2, 5])
-pattern_3: Iterable[int] = cycle([3, 3, 1, 1, 2, 2, 4, 4, 5, 5])
-
-patterns = [pattern_1, pattern_2, pattern_3]
 
 def solution(answers: List[int]) -> List[int]:
+    patterns: List[Iterable[int]] = [
+        cycle([1, 2, 3, 4, 5]),
+        cycle([2, 1, 2, 3, 2, 4, 2, 5]),
+        cycle([3, 3, 1, 1, 2, 2, 4, 4, 5, 5])
+    ]
     score: List[int] = [0, 0, 0]
 
     for i in range(len(score)):
