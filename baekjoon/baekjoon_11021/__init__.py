563494b2ddeb09b55a6de366749fba3131e3a5fe
Suh Junmin <sof.midnight@live.co.kr>
2021-03-14 15:29:05 +0900

    add baekjoon 11021 11022

---
+++ baekjoon/baekjoon_11021/__init__.py
@@ -0,0 +1,12 @@
+from typing import *
+
+
+def solution() -> None:
+    num: int = int(input().strip())
+
+    for i in range(1, num + 1):
+        a: int
+        b: int
+        a, b = map(int, input().strip().split())
+
+        print("Case #{}: {}".format(i, a + b))
---
+++ baekjoon/baekjoon_11022/tests/test_baekjoon_11022.py
@@ -0,0 +1,31 @@
+from typing import *
+from unittest import mock
+
+import pytest
+
+from .. import solution
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        [
+            ["5", "1 1", "2 3", "3 4", "9 8", "5 2"],
+            [
+                "Case #1: 1 + 1 = 2",
+                "Case #2: 2 + 3 = 5",
+                "Case #3: 3 + 4 = 7",
+                "Case #4: 9 + 8 = 17",
+                "Case #5: 5 + 2 = 7",
+            ],
+        ],
+    ],
+)
+def test_solution(capsys, test_input: List[str], expected: List[str]) -> None:
+    iter_input: Iterator = iter(test_input)
+
+    with mock.patch("builtins.input", lambda: next(iter_input) + "\n"):
+        solution()
+        captured = capsys.readouterr()
+
+        assert captured.out == "\n".join(expected) + "\n"
